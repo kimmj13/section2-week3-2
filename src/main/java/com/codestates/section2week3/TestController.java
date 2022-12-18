48793109c6f18829bc03ce6bd0b7a2b45fb68351
kimmj13 <110887976+kimmj13@users.noreply.github.com>
2022-12-18 12:08:49 +0900

    TestController - url 수정 (#11)
    * :sparkles: feat : TestController
    
    * :sparkles: feat : TestController-test2 추가
    
    * :bug: fix: url 변경
    
    test1, test2 중복 url 변경

--- src/main/java/com/codestates/section2week3/TestController.java
+++ src/main/java/com/codestates/section2week3/TestController.java
@@ -6,12 +6,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
-    @GetMapping
+    @GetMapping("/test1")
     public String test1() {
         return "test1";
     }
 
-    @GetMapping
+    @GetMapping("/test2")
     public String test2() {
         return "test2";
     }
